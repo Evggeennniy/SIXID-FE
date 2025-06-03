5f4eac808521366bf46f380dbad81806d93b1c6f
Polina Lozova <1polinalozovaya1@gmail.com>
2025-06-03 13:54:45 +0300

    add authPage

---
+++ src/pages/AuthPage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthApp from "@widgets/AuthApp/index.jsx";
+
+function AuthPage() {
+	return (
+		<AuthApp/>
+	);
+}
+
+export default AuthPage;
---
+++ src/widgets/AuthApp/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthForm from "@widgets/AuthApp/ui/AuthForm.jsx";
+
+function AuthApp() {
+  return (
+    <section className='flex justify-center items-center mx-auto'>
+      <AuthForm />
+    </section>
+  );
+}
+
+export default AuthApp;
